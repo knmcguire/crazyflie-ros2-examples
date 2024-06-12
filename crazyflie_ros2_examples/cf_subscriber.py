c136e3565a7902a33d042fd916a0047a80447361
knmcguire <kimberly@bitcraze.io>
2024-06-12 13:40:45 +0200

    subscriber to twists

--- crazyflie_ros2_examples/cf_subscriber.py
+++ crazyflie_ros2_examples/cf_subscriber.py
@@ -2,13 +2,16 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from std_msgs.msg import Float32
+from geometry_msgs.msg import Twist
 
 import logging
 
 import cflib.crtp  # noqa
 from cflib.crazyflie import Crazyflie
 
-##ros2 topic pub /topic std_msgs/msg/String '{data: "hello"}'
+# ros2 topic pub /topic std_msgs/msg/String '{data: "hello"}''
+# ros2 topic pub --once /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -22,14 +25,18 @@ class CrazyflieSubscriber(Node):
 
         super().__init__('crazyflie_subscriber')
         self.subscription = self.create_subscription(
-            String,
-            'topic',
+            Twist,
+            'cmd_vel',
             self._listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
+        self.fixed_height = 0.3
+
     def _listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.get_logger().info('I heard: "%s"' % msg)
+        self._cf.commander.send_hover_setpoint(msg.linear.x, msg.linear.y, msg.angular.z,  self.fixed_height)
+
 
     def _connected(self, link_uri):
         self.get_logger().info('Connected to %s' % link_uri)
